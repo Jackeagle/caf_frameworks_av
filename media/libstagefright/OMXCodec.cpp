gp-b2g/frameworks_base

libstagefright: Ignore port settings changed event in certain cases
Ignore port settings changed event if the client has already
requested the component to move to Idle state (or any other state)
from Executing state. This scenario happens when Stop is called
immediately after Stop on OMXCodec. Since, Stop is called
so we request the component to move into Idle state and set
the state variable accordingly. In this case we might receive
port settings changed event after we have requested the
component to move into Idle state.

Change-Id: I35cb15bb582381c86f327cd874999ac05c771ee2

media/libstagefright/OMXCodec.cpp
@@ -2904,6 +2904,12 @@ void OMXCodec::onEvent(OMX_EVENTTYPE event, OMX_U32 data1, OMX_U32 data2) {
         {
             CODEC_LOGV("OMX_EventPortSettingsChanged(port=%ld, data2=0x%08lx)",
                        data1, data2);
+
+            if (mState != EXECUTING) {
+                LOGE("Ignore PortSettingsChanged event");
+                break;
+            }
+
             if (data2 == 0 || data2 == OMX_IndexParamPortDefinition) {
                 // There is no need to check whether mFilledBuffers is empty or not
                 // when the OMX_EventPortSettingsChanged is not meant for reallocating

Jackeagle/caf_frameworks_av

media/libstagefright/OMXCodec.cpp
@@ -2472,6 +2472,11 @@ void OMXCodec::onEvent(OMX_EVENTTYPE event, OMX_U32 data1, OMX_U32 data2) {
             CODEC_LOGV("OMX_EventPortSettingsChanged(port=%ld, data2=0x%08lx)",
                        data1, data2);
 
+            if (mState != EXECUTING) {
+                CODEC_LOGE("Ignore PortSettingsChanged event");
+                break;
+            }
+
             if (data2 == 0 || data2 == OMX_IndexParamPortDefinition) {
                 // There is no need to check whether mFilledBuffers is empty or not
                 // when the OMX_EventPortSettingsChanged is not meant for reallocating