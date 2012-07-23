gp-b2g/frameworks_base

libstagefright: best effort for udta atom errors
- udta atoms define user data which is not crucial to playback
- skips over errors in the udta atom should any be encountered
  to attempt playback

Change-Id: I9d9d3ff17e31ab0eb8484cce38cf92f91c1292c3
Crs-Fixed: 333572

media/libstagefright/MPEG4Extractor.cpp
@@ -749,7 +749,12 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
             while (*offset < stop_offset) {
                 status_t err = parseChunk(offset, depth + 1);
                 if (err != OK) {
-                    return err;
+                    if(chunk_type == FOURCC('u', 'd', 't', 'a')){
+                        LOGW("error in udta atom, ignoring %llu bytes",stop_offset - *offset);
+                        *offset = stop_offset;
+                    } else {
+                        return err;
+                    }
                 }
             }
 

Jackeagle/caf_frameworks_av

media/libstagefright/MPEG4Extractor.cpp
@@ -739,7 +739,12 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
             while (*offset < stop_offset) {
                 status_t err = parseChunk(offset, depth + 1);
                 if (err != OK) {
-                    return err;
+                    if(chunk_type == FOURCC('u', 'd', 't', 'a')){
+                        ALOGW("error in udta atom, ignoring %llu bytes",stop_offset - *offset);
+                        *offset = stop_offset;
+                    } else {
+                        return err;
+                    }
                 }
             }
 