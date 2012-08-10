d03b81a941f2d6abfeea580918cb254950a07337
Mingming Yin <mingming@codeaurora.org>
2012-08-10 14:33:17 -0700

    Add AUDIO_CHANNEL_IN_5POINT1 as supported input channel.
    Change-Id: I40f4be2e55f922b7fd46bea05d391ce7d3395b5c

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -219,8 +219,8 @@ out:
 }
 
 static uint32_t getInputChannelCount(uint32_t channels) {
-   // only mono or stereo is supported for input sources
-   return popcount((channels) & (AUDIO_CHANNEL_IN_STEREO | AUDIO_CHANNEL_IN_MONO));
+   // only mono or stereo and 5.1 are supported for input sources
+   return popcount((channels) & (AUDIO_CHANNEL_IN_STEREO | AUDIO_CHANNEL_IN_MONO | AUDIO_CHANNEL_IN_5POINT1));
 }
 // ----------------------------------------------------------------------------
 
