deb69d2780753cc4c3a053a57e0a9ca8f9295f73
Ashish Jain <ashishj@codeaurora.org>
2012-08-12 19:04:19 +0530

    libstagefright: changes for .cpt and .dtshd
    Adding file types for .cpt and .dtshd
    CRs-Fixed: 377122
    
    Change-Id: Ib6d9030e9bcfd7b2ea9cbd3cb42ac38a4c7ad68d

--- media/libstagefright/StagefrightMediaScanner.cpp
+++ media/libstagefright/StagefrightMediaScanner.cpp
@@ -43,7 +43,8 @@ static bool FileHasAcceptableExtension(const char *extension) {
         ".mpeg", ".ogg", ".mid", ".smf", ".imy", ".wma", ".aac",
         ".wav", ".amr", ".midi", ".xmf", ".rtttl", ".rtx", ".ota",
         ".mkv", ".mka", ".webm", ".ts", ".fl", ".flac", ".mxmf",
-        ".avi", ".mpg", ".qcp", ".awb", ".ac3", ".dts", ".wmv", ".ec3"
+        ".avi", ".mpg", ".qcp", ".awb", ".ac3", ".dts", ".wmv", ".ec3",
+        ".cpt", ".dtshd"
     };
     static const size_t kNumValidExtensions =
         sizeof(kValidExtensions) / sizeof(kValidExtensions[0]);
