ca657fc13734fcd154abbec2084cb68a23abfb69
Phani Kumar Allada <pallad@codeaurora.org>
2012-09-25 15:58:16 +0530

    framework/av: Fix for frame stuck at the end of video playback
    - After audio is ended in the video clip, the reset of the video
    is not played and the video frame gets stuck.
    
    - Audioplayer delays the EOS propagation to AwesomePlayer until
    all the pending pcm samples are played out even though EOS occurred
    at output port. Until EOS sent, awesome player uses AudioPlayer as
    time source. But as EOS on Audioplayer side already occurred,
    timestamp doesn't get updated. This delays video frame rendering.
    when all the pcm samples are played out, EOS is generated by Audio
    player. At this point time source switches from Audioplayer to System
    clock, forcing video to render pending frames immediately as it finds
    video frames are delayed now.
    
    - When AudioPlayer receives EOS from OMXCodec, signal AwesomePlayer
    with postAudioEOS event, which will set the mAudioEOD flag to true.
    In onVideoEvent() function, AudioPlayer is set as time source only if
    mAudioEOD flag is not set. By setting the FIRST_FRAME with audioEOS
    event, the time source difference is taken into account while lateness
    calculation.
    
    Change-Id: I217803a4b0ea79317a87a39204d1415110b9252f
    CRs-Fixed: 399923
    (cherry picked from commit 4177026b56c9af17ad75189b8fc42349c7ba1e3a)

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -197,6 +197,7 @@ AwesomePlayer::AwesomePlayer()
       mUIDValid(false),
       mTimeSource(NULL),
       mVideoRendererIsPreview(false),
+      mAudioEOD(false),
       mAudioPlayer(NULL),
       mDisplayWidth(0),
       mDisplayHeight(0),
@@ -1770,6 +1771,7 @@ void AwesomePlayer::finishSeekIfNecessary(int64_t videoTimeUs) {
         // If we don't have a video time, seek audio to the originally
         // requested seek time instead.
 
+        mAudioEOD = false;
         mAudioPlayer->seekTo(videoTimeUs < 0 ? mSeekTimeUs : videoTimeUs);
         mWatchForAudioSeekComplete = true;
         mWatchForAudioEOS = true;
@@ -1975,7 +1977,7 @@ void AwesomePlayer::onVideoEvent() {
     }
 
     TimeSource *ts =
-        ((mFlags & AUDIO_AT_EOS) || !(mFlags & AUDIOPLAYER_STARTED))
+        ((mFlags & AUDIO_AT_EOS) || !(mFlags & AUDIOPLAYER_STARTED) || (mAudioEOD == true))
             ? &mSystemTimeSource : mTimeSource;
 
     if (mFlags & FIRST_FRAME) {
@@ -1996,7 +1998,8 @@ void AwesomePlayer::onVideoEvent() {
 
     int64_t realTimeUs, mediaTimeUs, nowUs = 0, latenessUs = 0;
     if (!(mFlags & AUDIO_AT_EOS) && mAudioPlayer != NULL
-        && mAudioPlayer->getMediaTimeMapping(&realTimeUs, &mediaTimeUs)) {
+        && mAudioPlayer->getMediaTimeMapping(&realTimeUs, &mediaTimeUs)
+        && (mAudioEOD == false)) {
         mTimeSourceDeltaUs = realTimeUs - mediaTimeUs;
     }
 
@@ -2216,7 +2219,6 @@ void AwesomePlayer::onCheckAudioStatus() {
     if (mWatchForAudioEOS && mAudioPlayer->reachedEOS(&finalStatus)) {
         mWatchForAudioEOS = false;
         modifyFlags(AUDIO_AT_EOS, SET);
-        modifyFlags(FIRST_FRAME, SET);
         postStreamDoneEvent_l(finalStatus);
     }
 }
@@ -2568,6 +2570,8 @@ uint32_t AwesomePlayer::flags() const {
 }
 
 void AwesomePlayer::postAudioEOS(int64_t delayUs) {
+    mAudioEOD = true;
+    modifyFlags(FIRST_FRAME, SET);
     postCheckAudioStatusEvent(delayUs);
 }
 
