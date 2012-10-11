a98651f7b2ee44486d760d3569b3cecae2597fcd
Vikash Garodia <vgarodia@codeaurora.org>
2012-10-11 21:04:25 +0530

    libstagefright: reset flag to allow buffering
    When buffering is 100% or eos is reached, buffering flag
    is set.If the playback is seeked back to some location
    after EOS, no buffering happen. Without buffering, user experience
    is bad in poor network condition.
    This also handle file having header at the end. While reading
    header EOS get set and buffering is prevented.
    
    Reset the flag to allow buffering during above use cases.
    
    (cherry picked from commit 3f997cc16a6c75298565128a6b2dce211396e6ff)
    
    Change-Id: Ia4dd44c0310eecf81923cdc7368364f3397ad937
    CRs-Fixed: 409247

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -944,6 +944,11 @@ status_t AwesomePlayer::play_l() {
         }
     }
 
+    if(mBufferingDone) {
+        mBufferingDone = false;
+        postBufferingEvent_l();
+    }
+
     modifyFlags(PLAYING, SET);
     modifyFlags(FIRST_FRAME, SET);
 
@@ -1448,6 +1453,11 @@ status_t AwesomePlayer::seekTo(int64_t timeUs) {
 }
 
 status_t AwesomePlayer::seekTo_l(int64_t timeUs) {
+    if(mBufferingDone) {
+        mBufferingDone = false;
+        postBufferingEvent_l();
+    }
+
     if (mFlags & CACHE_UNDERRUN) {
         modifyFlags(CACHE_UNDERRUN, CLEAR);
         play_l();
