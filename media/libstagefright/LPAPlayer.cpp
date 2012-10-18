2109d7e21eb711620ea5e880aa8cabd6993737a7
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2012-10-18 14:02:03 +0530

    libstagefright: Fix for skip of lpa playback after pausetimeout
    -Playback of lpa clip skips for few seconds if pausetimeout occurs.
    -On pausetimeout stop is issued to the driver and flush is issued.
    When resumed, the previously sent buffers are skipped and started
    from a position much ahead.
    -Update mInternalSeeking in pausetimeout to make sure source is
    updated to the timeplayed before pause and resumes at correct position.
    
    Change-Id: Ifdb82a8bc72c89713214c24e2d6533c47fe525eb
    CRs-Fixed: 409613
    (cherry picked from commit 22ec3fb2f8c133b026160dd1c2091b076228f91c)

--- media/libstagefright/LPAPlayer.cpp
+++ media/libstagefright/LPAPlayer.cpp
@@ -436,6 +436,7 @@ void LPAPlayer::reset() {
     mPositionTimeMediaUs = -1;
     mPositionTimeRealUs = -1;
     mSeeking = false;
+    mInternalSeeking = false;
     mReachedEOS = false;
     mReachedOutputEOS = false;
     mFinalStatus = OK;
@@ -696,6 +697,7 @@ void LPAPlayer::onPauseTimeOut() {
         // 1.) Set seek flags
         mReachedEOS = false;
         mReachedOutputEOS = false;
+        mInternalSeeking = true;
         mSeekTimeUs = mTimePlayed;
 
         // 2.) Close routing Session
