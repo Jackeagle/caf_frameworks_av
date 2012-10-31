0449802777e0760de1021841c41ec659bd623e3d
Shashi Kumar <sulege@codeaurora.org>
2012-10-31 12:38:05 +0530

    Frameworks/av: Fix for mediaserver crash in AudioFlinger
    - UI freeze is observed while playing LPA clips with sound effects.
    - While moving to next clip, effect process is still applying
      effects on last buffers. Meanwhile de-allocating the local buffers
      in DirectAudioTrack destructor on which effects are currently
      applying. Due to this crash is observed in effect process.
    - De-allocate buffers once the effects thread is killed.
    
    CRs-Fixed: 416093
    (cherry picked from commit 18a6a4641bcb2287fc20d9876a2feeed9978844c)
    
    Change-Id: Id9e0b90340780fc5446f3417bc411675ccf64264

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -6228,10 +6228,10 @@ AudioFlinger::DirectAudioTrack::~DirectAudioTrack() {
     POSTPRO_PATCH_ICS_OUTPROC_DIRECT_EXIT(this, gettid());
 #endif
     if (mFlag & AUDIO_OUTPUT_FLAG_LPA) {
-        deallocateBufPool();
         requestAndWaitForEffectsThreadExit();
         mAudioFlinger->deregisterClient(mAudioFlingerClient);
         mAudioFlinger->deleteEffectSession();
+        deallocateBufPool();
     }
     releaseWakeLock();
 
