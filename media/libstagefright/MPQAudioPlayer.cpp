f54ca9bb0ffd17db2f227997e5ff0a128f9f629d
Aviral Gupta <aviralg@codeaurora.org>
2012-11-23 00:00:50 +0530

    libstagefright: Remove the 0 byte write to driver at EOS
    Since the break in the compressed driver is tracked by the
    app and hw pointer so the 0 byte write hack is no more needed.
    This change caused an issue when writing of 0 bytes is taking time
    resulting in the miss of the signal to break out of the lock.
    
    Change-Id: Ida4fff69575750d365ba9d0ad8ecc1fec521b341

--- media/libstagefright/MPQAudioPlayer.cpp
+++ media/libstagefright/MPQAudioPlayer.cpp
@@ -668,10 +668,6 @@ void MPQAudioPlayer::extractorThreadEntry() {
                 ALOGV("Posting EOS event..zero byte buffer ");
                 //TODO : make it POST EOS to amke sense for  Software
                 if(!mPostedEOS) {
-                    if( mDecoderType == EMS11Decoder) {
-                        memset(mLocalBuf, 0x0, AAC_AC3_BUFFER_SIZE);
-                        mAudioSink->write(mLocalBuf, 0);
-                    }
                     mPostedEOS = true;
                     mObserver->postAudioEOS( mPostEOSDelayUs);
                 }
@@ -681,8 +677,6 @@ void MPQAudioPlayer::extractorThreadEntry() {
         else if (mDecoderType == EHardwareDecoder) {
             mExtractorMutex.lock();
             if (mReachedExtractorEOS || mIsPaused || mAsyncReset ) {
-                if (mReachedExtractorEOS)
-                    mAudioSink->write(mLocalBuf, 0);
                 ALOGV("extractorThreadEntry: mIsPaused %d\
                         mReachedExtractorEOS %d mAsyncReset %d ",\
                         mIsPaused, mReachedExtractorEOS, mAsyncReset);
