05706449b64d3bbe1f731b54b149e72095789280
Riaz Rahaman <riazr@codeaurora.org>
2012-12-05 11:09:08 +0530

    httplive: Buffer size & bandwidth switch enhancements for fetchFile
    - Adds size estimation for chunked ts files so that buffer sizes are
      more accurate and fewer calls to memcpy are needed
    - Fixes case where even files with size returned by the server caused
      a memcpy
    - Increases minimum buffer increment and allows exponential buffer
      growth by a factor of 50% to reduce memcpy calls
    - Splits the readAt call in fetchFile so that the bandwidth can be
      checked during download
    - Adapts the incremental buffer read size to the current network
      conditions
    - Returns a notification that triggers the download of a lower
      quality file if the bandwidth drops by enough. Will only begin
      downloading a lower quality file if it will save time and thus
      minimize any apparent media freeze while buffering.
    
    Change-Id: I31f76cafbfdbab3b491f63be8df9eb5fdb68f7d5
    CRs-Fixed: 329809,416098

--- media/libstagefright/include/LiveSession.h
+++ media/libstagefright/include/LiveSession.h
@@ -92,11 +92,13 @@ private:
     KeyedVector<AString, sp<ABuffer> > mAESKeyForURI;
 
     ssize_t mPrevBandwidthIndex;
+    ssize_t mBandwidthIndex;
     int64_t mLastPlaylistFetchTimeUs;
     sp<M3UParser> mPlaylist;
     int32_t mSeqNumber;
     int64_t mSeekTimeUs;
     int32_t mNumRetries;
+    int64_t mPrevBufferSize;
 
     Mutex mLock;
     Condition mCondition;
@@ -126,10 +128,11 @@ private:
 
     status_t fetchFile(
             const char *url, sp<ABuffer> *out,
-            int64_t range_offset = 0, int64_t range_length = -1);
+            int64_t range_offset = 0, int64_t range_length = -1, bool isMedia = true);
 
     sp<M3UParser> fetchPlaylist(const char *url, bool *unchanged);
     size_t getBandwidthIndex();
+    void estimateFilesize(off64_t *size);
 
     status_t decryptBuffer(
             size_t playlistIndex, const sp<ABuffer> &buffer);
