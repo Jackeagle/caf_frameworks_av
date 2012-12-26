18d03a3b2db02ef8a9c55a1302fbc12ff98b0a8c
Shashi Kumar <sulege@codeaurora.org>
2012-12-26 18:55:38 +0530

    frameworks/av: Fix to play previous non-LPA clip which uses hardware decoder
    - Fails to play previous non-LPA clip with hardware decoder if next clip
      is LPA and current non-LPA clip is playing with software decoder.
    - While playing current non-LPA clip with software decoder and if the next
      clip is LPA, the LPA in progress flag is set to true. Here, when selecting
      previous non-LPA clip which uses only hardware decoder fails to play as
      LPA in progress flag doesn't allow to use hardware decoder.
    - Resetting LPA in progress flag to false if next clip is LPA, but not
      yet started playing.
    
    Change-Id: I00fd57d837f50b56e8213635be65d024c821cbb3
    CRs-Fixed: 432580

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -211,7 +211,8 @@ AwesomePlayer::AwesomePlayer()
       mDecryptHandle(NULL),
       mLastVideoTimeUs(-1),
       mTextDriver(NULL),
-      mBufferingDone(false) {
+      mBufferingDone(false),
+      mIsLPASession(false) {
     CHECK_EQ(mClient.connect(), (status_t)OK);
 
     DataSource::RegisterDefaultSniffers();
@@ -598,6 +599,14 @@ void AwesomePlayer::reset_l() {
     mAudioSource.clear();
 
     mTimeSource = NULL;
+
+#ifndef NON_QCOM_TARGET
+    if(LPAPlayer::objectsAlive == 0 && mIsLPASession == true)
+    {
+        LPAPlayer::mLpaInProgress = false;
+        mIsLPASession = false;
+    }
+#endif
     delete mAudioPlayer;
 
     mAudioPlayer = NULL;
@@ -1653,6 +1662,7 @@ status_t AwesomePlayer::initAudioDecoder() {
             flags |= OMXCodec::kSoftwareCodecsOnly;
             if(mDurationUs > 60000000) {
                LPAPlayer::mLpaInProgress = true;
+               mIsLPASession = true;
             }
         }
 #endif
