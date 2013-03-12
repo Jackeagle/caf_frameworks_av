584ea6c724f5c91414392074f22bf9e4dc863474
huiy <huiyQuic@codeaurora.org>
2013-03-11 19:19:47 -0700

    Add 2 APIs (suspend and resume) in Mediaplayer
    - API:suspend() will just pause the player and release all the decoders
    - instead of release the whole player
    - API:resume() will just init the decoders again
    - Add a check in onVideoEvent()
    - to make sure the first seek operation will seek the next i-frame
    
    Change-Id: Iefa77efafaba92bc266bd95744be7b76bd1372f8

--- include/media/MediaPlayerInterface.h
+++ include/media/MediaPlayerInterface.h
@@ -201,6 +201,9 @@ public:
         return INVALID_OPERATION;
     }
 
+    virtual status_t suspend() { return INVALID_OPERATION; }
+    virtual status_t resume() { return INVALID_OPERATION; }
+
 private:
     friend class MediaPlayerService;
 
