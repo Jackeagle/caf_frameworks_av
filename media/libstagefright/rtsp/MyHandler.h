a54176d836724a447b0549d9ef3eaacfaa6e8631
Surajit Podder <spodder@codeaurora.org>
2013-04-16 14:44:27 +0530

    rtsp: Add a property "rtsp.transport.TCP"
    - Add property "rtsp.transport.TCP"
      Value "true" - TCP
      Value "false" - UDP
      default value - false
    
    Change-Id: Ic27b440067af49e7e3bcc45d86eb7a17ae9db54f
    CRs-Fixed: 435538

--- media/libstagefright/rtsp/MyHandler.h
+++ media/libstagefright/rtsp/MyHandler.h
@@ -28,6 +28,7 @@
 #include "ASessionDescription.h"
 
 #include <ctype.h>
+#include <cutils/properties.h>
 
 #include <media/stagefright/foundation/ABuffer.h>
 #include <media/stagefright/foundation/ADebug.h>
@@ -132,6 +133,14 @@ struct MyHandler : public AHandler {
         mNetLooper->start(false /* runOnCallingThread */,
                           false /* canCallJava */,
                           PRIORITY_HIGHEST);
+
+        char value[PROPERTY_VALUE_MAX];
+        property_get("rtsp.transport.TCP", value, "false");
+        if (!strcmp(value, "true")) {
+            mTryTCPInterleaving = true;
+        } else {
+            mTryTCPInterleaving = false;
+        }
 
         // Strip any authentication info from the session url, we don't
         // want to transmit user/pass in cleartext.
