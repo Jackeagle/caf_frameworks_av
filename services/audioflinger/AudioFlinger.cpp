45780aa73756e7bb3936307371d390cee4e4d7a6
Jitendra Singh Naruka <jnaruk@codeaurora.org>
2013-07-05 18:08:27 -0700

    libstagefright: audioflinger: query for tunnel mode latency
    During tunnel playback, we need to set proper latency when
    audio is played on BT devices by querying BT hal.
    This is needed for av sync over BT devices.
    
    CRs-Fixed: 505359
    Change-Id: I7f49d1317754dcd5ed0e75349802dd80c446fe39
    (cherry picked from commit a808d2f78fc7c2857b2ec3b03baecd5a6b54cbea)

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -898,6 +898,13 @@ size_t AudioFlinger::frameCount(audio_io_handle_t output) const
 uint32_t AudioFlinger::latency(audio_io_handle_t output) const
 {
     Mutex::Autolock _l(mLock);
+    if (!mDirectAudioTracks.isEmpty()) {
+        AudioSessionDescriptor *desc = mDirectAudioTracks.valueFor(output);
+        if(desc != NULL) {
+            return desc->stream->get_latency(desc->stream);
+        }
+    }
+
     PlaybackThread *thread = checkPlaybackThread_l(output);
     if (thread == NULL) {
         ALOGW("latency() unknown thread %d", output);
@@ -2344,6 +2351,7 @@ uint32_t AudioFlinger::PlaybackThread::latency() const
     Mutex::Autolock _l(mLock);
     return latency_l();
 }
+
 uint32_t AudioFlinger::PlaybackThread::latency_l() const
 {
     if (initCheck() == NO_ERROR) {
