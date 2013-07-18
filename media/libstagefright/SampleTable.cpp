2b30dbc695fc80025f0b12a64c1f52caae105dc8
quic_xiaoqiny <quic_xiaoqiny@codeaurora.org>
2013-07-18 18:01:25 +0800

    SampleTable: Add shortcut in getMaxSampleSize() to avoid ANR in corrupted clips
    - for very large/corrupt values of mNumSampleSizes, getMaxSampleSize
      will run a very long forloop that will cause an ANR
    - for cases where mDefaultSampleSize is zero, this ANR issue will not
      occur becase a check is in place for checking mNumSampleSizes
      corruption
    - for cases where mDefaultSampleSize is non-zero, getMaxSampleSize will
      proceed to process each sample for finding max size. But each run of
      SampleIterator::getSampleSizeDirect will return mDefaultSampleSize as
      the size
    - a check for mDefaultSampleSize being non-zero is placed before the for
      loop, and will return mDefaultSampleSize if it is the case, skipping
      the entire for loop and avoids ANR
    
      CRs-Fixed: 316017
    
    Change-Id: I163deb30045f051cc76f153af7f28ca54bcff9e0

--- media/libstagefright/SampleTable.cpp
+++ media/libstagefright/SampleTable.cpp
@@ -283,13 +283,20 @@ status_t SampleTable::setSampleSizeParams(
     if (type == kSampleSizeType32) {
         mSampleSizeFieldSize = 32;
 
+       //this needs to be 64 or overflow may occur from the calculation
+        uint64_t expectedDataSize = (uint64_t)12 + (uint64_t)mNumSampleSizes * (uint64_t)4;
+        //mDefaultSampleSize = 0 means sample table follows the field
+        if(((uint64_t)data_size < expectedDataSize) && (mDefaultSampleSize == 0)) {
+         return ERROR_MALFORMED;
+        }
+       if(((uint64_t)data_size < expectedDataSize) && ((mDefaultSampleSize & 0xFF000000) !=0)) {
+         return ERROR_MALFORMED;
+       }
+
         if (mDefaultSampleSize != 0) {
             return OK;
         }
 
-        if (data_size < 12 + mNumSampleSizes * 4) {
-            return ERROR_MALFORMED;
-        }
     } else {
         if ((mDefaultSampleSize & 0xffffff00) != 0) {
             // The high 24 bits are reserved and must be 0.
@@ -444,6 +451,11 @@ status_t SampleTable::getMaxSampleSize(size_t *max_size) {
     Mutex::Autolock autoLock(mLock);
 
     *max_size = 0;
+
+    if(mDefaultSampleSize > 0) {
+       *max_size = mDefaultSampleSize;
+        return OK;
+    }
 
     for (uint32_t i = 0; i < mNumSampleSizes; ++i) {
         size_t sample_size;
