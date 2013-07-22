ca12f04e4dca337631fa44968c4e7909ffd0a234
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2013-07-22 13:57:45 +0530

    audioflinger: Fix for race conditions in direct audio track
    -Mediaserver crash is observed in direct track functions.
    -Between write and flush functions, write and destructor effectpool
    buffers are accessed without any synchronization that might lead to
    operations on null buffers and cause crash.
    -Use mEffectLock to synchronize access of effectpool buffers.
    
    Change-Id: Ib071361c65b390f9486c0a080a9391f176392b30
    CRs-Fixed: 515107

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -6378,8 +6378,8 @@ ssize_t AudioFlinger::DirectAudioTrack::write(const void *buffer, size_t size) {
         return 0;
     }
 
-    if (mFlag & AUDIO_OUTPUT_FLAG_LPA) {
-        mEffectLock.lock();
+    mEffectLock.lock();
+    if ((mFlag & AUDIO_OUTPUT_FLAG_LPA) && !mEffectsPool.empty()) {
         List<BufferInfo>::iterator it = mEffectsPool.begin();
         BufferInfo buf = *it;
         mEffectsPool.erase(it);
@@ -6388,16 +6388,18 @@ ssize_t AudioFlinger::DirectAudioTrack::write(const void *buffer, size_t size) {
         mEffectsPool.push_back(buf);
         mAudioFlinger->applyEffectsOn(static_cast<void *>(this),
             (int16_t*)buf.localBuf, (int16_t*)buffer, (int)size, true);
-        mEffectLock.unlock();
     }
+    mEffectLock.unlock();
     ALOGV("out of Writing to AudioSessionOut");
     return mOutputDesc->stream->write(mOutputDesc->stream, buffer, size);
 }
 
 void AudioFlinger::DirectAudioTrack::flush() {
     if (mFlag & AUDIO_OUTPUT_FLAG_LPA) {
+        mEffectLock.lock();
         mEffectsPool.clear();
         mEffectsPool = mBufPool;
+        mEffectLock.unlock();
     }
     mOutputDesc->stream->flush(mOutputDesc->stream);
 }
@@ -6478,6 +6480,7 @@ void AudioFlinger::DirectAudioTrack::deallocateBufPool() {
 
     //1. Deallocate the local memory
     //2. Remove all the buffers from bufpool
+    mEffectLock.lock();
     while (!mBufPool.empty())  {
         List<BufferInfo>::iterator it = mBufPool.begin();
         BufferInfo &memBuffer = *it;
@@ -6489,6 +6492,8 @@ void AudioFlinger::DirectAudioTrack::deallocateBufPool() {
         ALOGV("Removing from bufpool");
         mBufPool.erase(it);
     }
+    mEffectsPool.clear();
+    mEffectLock.unlock();
 
     free(mEffectsThreadScratchBuffer);
     mEffectsThreadScratchBuffer = NULL;
