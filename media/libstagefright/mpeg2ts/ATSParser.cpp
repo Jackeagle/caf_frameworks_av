DolbyLaboratories/caf-platform-frameworks-av

libstagefright: Add Dolby UDC decoder support
Add support for ARM based AC3/EAC3 decoder

Change-Id: I63e994fca23479ea9f7c68d5f008eed00cbafffd

media/libstagefright/mpeg2ts/ATSParser.cpp
@@ -12,6 +12,25 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file was modified by Dolby Laboratories, Inc. The portions of the
+ * code that are surrounded by "DOLBY..." are copyrighted and
+ * licensed separately, as follows:
+ *
+ *  (C) 2011-2012 Dolby Laboratories, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 //#define LOG_NDEBUG 0
@@ -502,6 +521,19 @@ ATSParser::Stream::Stream(
                     ElementaryStreamQueue::PCM_AUDIO);
             break;
 
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+        case STREAMTYPE_DDP_AC3_AUDIO:
+            // TODO FIXME verify!
+            mQueue = new ElementaryStreamQueue(
+                    ElementaryStreamQueue::DDP_AC3_AUDIO);
+            break;
+
+        case STREAMTYPE_DDP_EC3_AUDIO:
+            // TODO FIXME verify!
+            mQueue = new ElementaryStreamQueue(
+                    ElementaryStreamQueue::DDP_EC3_AUDIO);
+            break;
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
         default:
             break;
     }
@@ -600,6 +632,10 @@ bool ATSParser::Stream::isAudio() const {
         case STREAMTYPE_MPEG2_AUDIO:
         case STREAMTYPE_MPEG2_AUDIO_ADTS:
         case STREAMTYPE_PCM_AUDIO:
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+        case STREAMTYPE_DDP_AC3_AUDIO:
+        case STREAMTYPE_DDP_EC3_AUDIO:
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
             return true;
 
         default:

media/libstagefright/mpeg2ts/ATSParser.h
@@ -12,6 +12,25 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file was modified by Dolby Laboratories, Inc. The portions of the
+ * code that are surrounded by "DOLBY..." are copyrighted and
+ * licensed separately, as follows:
+ *
+ *  (C) 2011-2012 Dolby Laboratories, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 #ifndef A_TS_PARSER_H_
@@ -88,6 +107,10 @@ struct ATSParser : public RefBase {
         STREAMTYPE_MPEG4_VIDEO          = 0x10,
         STREAMTYPE_H264                 = 0x1b,
         STREAMTYPE_PCM_AUDIO            = 0x83,
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+        STREAMTYPE_DDP_AC3_AUDIO        = 0x81,
+        STREAMTYPE_DDP_EC3_AUDIO        = 0x87,
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
     };
 
 protected:

media/libstagefright/mpeg2ts/ESQueue.cpp
@@ -12,6 +12,25 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file was modified by Dolby Laboratories, Inc. The portions of the
+ * code that are surrounded by "DOLBY..." are copyrighted and
+ * licensed separately, as follows:
+ *
+ *  (C) 2011-2012 Dolby Laboratories, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 //#define LOG_NDEBUG 0
@@ -120,6 +139,14 @@ static bool IsSeeminglyValidMPEGAudioHeader(const uint8_t *ptr, size_t size) {
     return true;
 }
 
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+static bool IsSeeminglyValidDDPAudioHeader(const uint8_t *ptr, size_t size) {
+    if (size < 2) return false;
+    if (ptr[0] == 0x0b && ptr[1] == 0x77) return true;
+    if (ptr[0] == 0x77 && ptr[1] == 0x0b) return true;
+    return false;
+}
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
 status_t ElementaryStreamQueue::appendData(
         const void *data, size_t size, int64_t timeUs) {
     if (mBuffer == NULL || mBuffer->size() == 0) {
@@ -255,6 +282,36 @@ status_t ElementaryStreamQueue::appendData(
                 break;
             }
 
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+            case DDP_AC3_AUDIO:
+            case DDP_EC3_AUDIO:
+            {
+                uint8_t *ptr = (uint8_t *)data;
+
+                ssize_t startOffset = -1;
+                for (size_t i = 0; i < size; ++i) {
+                    if (IsSeeminglyValidDDPAudioHeader(&ptr[i], size - i)) {
+                        startOffset = i;
+                        break;
+                    }
+                }
+
+                if (startOffset < 0) {
+                    return ERROR_MALFORMED;
+                }
+
+                if (startOffset > 0) {
+                    ALOGI("found something resembling a DDP audio "
+                         "syncword at offset %ld",
+                         startOffset);
+                }
+
+                data = &ptr[startOffset];
+                size -= startOffset;
+                break;
+            }
+
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
             default:
                 TRESPASS();
                 break;
@@ -333,6 +390,11 @@ sp<ABuffer> ElementaryStreamQueue::dequeueAccessUnit() {
             return dequeueAccessUnitMPEG4Video();
         case PCM_AUDIO:
             return dequeueAccessUnitPCMAudio();
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+        case DDP_AC3_AUDIO:
+        case DDP_EC3_AUDIO:
+            return dequeueAccessUnitDDP();
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
         default:
             CHECK_EQ((unsigned)mMode, (unsigned)MPEG_AUDIO);
             return dequeueAccessUnitMPEGAudio();
@@ -481,6 +543,99 @@ sp<ABuffer> ElementaryStreamQueue::dequeueAccessUnitAAC() {
     return accessUnit;
 }
 
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+static int
+calc_dd_frame_size(int code)
+{
+    /* tables lifted from TrueHDDecoder.cxx in DMG's decoder framework */
+    static const int FrameSize32K[] = { 96, 96, 120, 120, 144, 144, 168, 168, 192, 192, 240, 240, 288, 288, 336, 336, 384, 384, 480, 480, 576, 576, 672, 672, 768, 768, 960, 960, 1152, 1152, 1344, 1344, 1536, 1536, 1728, 1728, 1920, 1920 };
+    static const int FrameSize44K[] = { 69, 70, 87, 88, 104, 105, 121, 122, 139, 140, 174, 175, 208, 209, 243, 244, 278, 279, 348, 349, 417, 418, 487, 488, 557, 558, 696, 697, 835, 836, 975, 976, 114, 1115, 1253, 1254, 1393, 1394 };
+    static const int FrameSize48K[] = { 64, 64, 80, 80, 96, 96, 112, 112, 128, 128, 160, 160, 192, 192, 224, 224, 256, 256, 320, 320, 384, 384, 448, 448, 512, 512, 640, 640, 768, 768, 896, 896, 1024, 1024, 1152, 1152, 1280, 1280 };
+
+    int fscod = (code >> 6) & 0x3;
+    int frmsizcod = code & 0x3f;
+
+    if (fscod == 0) return 2 * FrameSize48K[frmsizcod];
+    if (fscod == 1) return 2 * FrameSize44K[frmsizcod];
+    if (fscod == 2) return 2 * FrameSize32K[frmsizcod];
+
+    return 0;
+}
+
+sp<ABuffer> ElementaryStreamQueue::dequeueAccessUnitDDP() {
+    unsigned int size;
+    unsigned char* ptr;
+    unsigned int i;
+    int bsid;
+    size_t frame_size = 0;
+    size_t auSize = 0;
+
+    size = mBuffer->size();
+    ptr = mBuffer->data();
+
+    /* parse the header */
+    if(size <= 6)
+    {
+        return NULL;
+    }
+
+    if(mFormat == NULL)
+    {
+        sp<MetaData> meta = new MetaData;
+        //TODO: Shoud this be EC3 or AC3 or if statement?
+        meta->setCString(kKeyMIMEType, MEDIA_MIMETYPE_AUDIO_EC3);
+
+        // Zero values entered to prevent crash
+        int32_t sampleRate = 0;
+        int32_t numChannels = 0;
+        meta->setInt32(kKeySampleRate, sampleRate);
+        meta->setInt32(kKeyChannelCount, numChannels);
+
+        mFormat = meta;
+    }
+
+    bsid = (ptr[5] >> 3) & 0x1f;
+    if (bsid > 10 && bsid <= 16)
+    {
+        frame_size = 2 * ((((ptr[2] << 8) | ptr[3]) & 0x7ff) + 1);
+    }
+    else
+    {
+        frame_size = calc_dd_frame_size(ptr[4]);
+    }
+
+    if (size < frame_size) {
+        ALOGW("Buffer size insufficient for frame size");
+        return NULL;
+    }
+
+    auSize += frame_size;
+
+    // Make Timestamp
+    int64_t timeUs = -1;
+    if(!mRangeInfos.empty())
+        timeUs = fetchTimestamp(frame_size);
+    else
+        ALOGW("Timestamp not created because mRangeInfos was empty");
+
+    // Now create an access unit
+    sp<ABuffer> accessUnit = new ABuffer(auSize);
+    // Put data into buffer
+    memcpy(accessUnit->data(), mBuffer->data(), frame_size);
+
+    memmove(mBuffer->data(), mBuffer->data() + frame_size, mBuffer->size() - frame_size);
+    mBuffer->setRange(0, mBuffer->size() - frame_size);
+
+    accessUnit->meta()->setInt64("timeUs", timeUs);
+    if (timeUs >= 0) {
+        accessUnit->meta()->setInt64("timeUs", timeUs);
+    } else {
+        ALOGW("no time for DDP access unit");
+    }
+
+    return accessUnit;
+}
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
 int64_t ElementaryStreamQueue::fetchTimestamp(size_t size) {
     int64_t timeUs = -1;
     bool first = true;

Jackeagle/caf_frameworks_av

media/libstagefright/mpeg2ts/ATSParser.cpp
@@ -12,6 +12,25 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file was modified by Dolby Laboratories, Inc. The portions of the
+ * code that are surrounded by "DOLBY..." are copyrighted and
+ * licensed separately, as follows:
+ *
+ *  (C) 2011-2012 Dolby Laboratories, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 //#define LOG_NDEBUG 0
@@ -506,6 +525,19 @@ ATSParser::Stream::Stream(
                     ElementaryStreamQueue::PCM_AUDIO);
             break;
 
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+        case STREAMTYPE_DDP_AC3_AUDIO:
+            // TODO FIXME verify!
+            mQueue = new ElementaryStreamQueue(
+                    ElementaryStreamQueue::DDP_AC3_AUDIO);
+            break;
+
+        case STREAMTYPE_DDP_EAC3_AUDIO:
+            // TODO FIXME verify!
+            mQueue = new ElementaryStreamQueue(
+                    ElementaryStreamQueue::DDP_EAC3_AUDIO);
+            break;
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
         default:
             break;
     }
@@ -614,6 +646,10 @@ bool ATSParser::Stream::isAudio() const {
         case STREAMTYPE_MPEG2_AUDIO:
         case STREAMTYPE_MPEG2_AUDIO_ADTS:
         case STREAMTYPE_PCM_AUDIO:
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+        case STREAMTYPE_DDP_AC3_AUDIO:
+        case STREAMTYPE_DDP_EAC3_AUDIO:
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
             return true;
 
         default:

media/libstagefright/mpeg2ts/ATSParser.h
@@ -12,6 +12,25 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file was modified by Dolby Laboratories, Inc. The portions of the
+ * code that are surrounded by "DOLBY..." are copyrighted and
+ * licensed separately, as follows:
+ *
+ *  (C) 2011-2012 Dolby Laboratories, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 #ifndef A_TS_PARSER_H_
@@ -89,6 +108,10 @@ struct ATSParser : public RefBase {
         STREAMTYPE_MPEG4_VIDEO          = 0x10,
         STREAMTYPE_H264                 = 0x1b,
         STREAMTYPE_PCM_AUDIO            = 0x83,
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+        STREAMTYPE_DDP_AC3_AUDIO        = 0x81,
+        STREAMTYPE_DDP_EAC3_AUDIO        = 0x87,
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
     };
 
 protected:

media/libstagefright/mpeg2ts/ESQueue.cpp
@@ -12,6 +12,25 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file was modified by Dolby Laboratories, Inc. The portions of the
+ * code that are surrounded by "DOLBY..." are copyrighted and
+ * licensed separately, as follows:
+ *
+ *  (C) 2011-2012 Dolby Laboratories, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 //#define LOG_NDEBUG 0
@@ -32,6 +51,10 @@
 #include "include/avc_utils.h"
 
 #include <netinet/in.h>
+
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+ #include <QCMediaDefs.h>
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
 
 namespace android {
 
@@ -120,6 +143,14 @@ static bool IsSeeminglyValidMPEGAudioHeader(const uint8_t *ptr, size_t size) {
     return true;
 }
 
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+static bool IsSeeminglyValidDDPAudioHeader(const uint8_t *ptr, size_t size) {
+    if (size < 2) return false;
+    if (ptr[0] == 0x0b && ptr[1] == 0x77) return true;
+    if (ptr[0] == 0x77 && ptr[1] == 0x0b) return true;
+    return false;
+}
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
 status_t ElementaryStreamQueue::appendData(
         const void *data, size_t size, int64_t timeUs) {
     if (mBuffer == NULL || mBuffer->size() == 0) {
@@ -256,6 +287,36 @@ status_t ElementaryStreamQueue::appendData(
                 break;
             }
 
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+            case DDP_AC3_AUDIO:
+            case DDP_EAC3_AUDIO:
+            {
+                uint8_t *ptr = (uint8_t *)data;
+
+                ssize_t startOffset = -1;
+                for (size_t i = 0; i < size; ++i) {
+                    if (IsSeeminglyValidDDPAudioHeader(&ptr[i], size - i)) {
+                        startOffset = i;
+                        break;
+                    }
+                }
+
+                if (startOffset < 0) {
+                    return ERROR_MALFORMED;
+                }
+
+                if (startOffset > 0) {
+                    ALOGI("found something resembling a DDP audio "
+                         "syncword at offset %ld",
+                         startOffset);
+                }
+
+                data = &ptr[startOffset];
+                size -= startOffset;
+                break;
+            }
+
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
             default:
                 TRESPASS();
                 break;
@@ -334,6 +395,11 @@ sp<ABuffer> ElementaryStreamQueue::dequeueAccessUnit() {
             return dequeueAccessUnitMPEG4Video();
         case PCM_AUDIO:
             return dequeueAccessUnitPCMAudio();
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+        case DDP_AC3_AUDIO:
+        case DDP_EAC3_AUDIO:
+            return dequeueAccessUnitDDP();
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
         default:
             CHECK_EQ((unsigned)mMode, (unsigned)MPEG_AUDIO);
             return dequeueAccessUnitMPEGAudio();
@@ -493,6 +559,99 @@ sp<ABuffer> ElementaryStreamQueue::dequeueAccessUnitAAC() {
     return accessUnit;
 }
 
+#if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
+static int
+calc_dd_frame_size(int code)
+{
+    /* tables lifted from TrueHDDecoder.cxx in DMG's decoder framework */
+    static const int FrameSize32K[] = { 96, 96, 120, 120, 144, 144, 168, 168, 192, 192, 240, 240, 288, 288, 336, 336, 384, 384, 480, 480, 576, 576, 672, 672, 768, 768, 960, 960, 1152, 1152, 1344, 1344, 1536, 1536, 1728, 1728, 1920, 1920 };
+    static const int FrameSize44K[] = { 69, 70, 87, 88, 104, 105, 121, 122, 139, 140, 174, 175, 208, 209, 243, 244, 278, 279, 348, 349, 417, 418, 487, 488, 557, 558, 696, 697, 835, 836, 975, 976, 114, 1115, 1253, 1254, 1393, 1394 };
+    static const int FrameSize48K[] = { 64, 64, 80, 80, 96, 96, 112, 112, 128, 128, 160, 160, 192, 192, 224, 224, 256, 256, 320, 320, 384, 384, 448, 448, 512, 512, 640, 640, 768, 768, 896, 896, 1024, 1024, 1152, 1152, 1280, 1280 };
+
+    int fscod = (code >> 6) & 0x3;
+    int frmsizcod = code & 0x3f;
+
+    if (fscod == 0) return 2 * FrameSize48K[frmsizcod];
+    if (fscod == 1) return 2 * FrameSize44K[frmsizcod];
+    if (fscod == 2) return 2 * FrameSize32K[frmsizcod];
+
+    return 0;
+}
+
+sp<ABuffer> ElementaryStreamQueue::dequeueAccessUnitDDP() {
+    unsigned int size;
+    unsigned char* ptr;
+    unsigned int i;
+    int bsid;
+    size_t frame_size = 0;
+    size_t auSize = 0;
+
+    size = mBuffer->size();
+    ptr = mBuffer->data();
+
+    /* parse the header */
+    if(size <= 6)
+    {
+        return NULL;
+    }
+
+    if(mFormat == NULL)
+    {
+        sp<MetaData> meta = new MetaData;
+        //TODO: Shoud this be EAC3 or AC3 or if statement?
+        meta->setCString(kKeyMIMEType, MEDIA_MIMETYPE_AUDIO_EAC3);
+
+        // Zero values entered to prevent crash
+        int32_t sampleRate = 0;
+        int32_t numChannels = 0;
+        meta->setInt32(kKeySampleRate, sampleRate);
+        meta->setInt32(kKeyChannelCount, numChannels);
+
+        mFormat = meta;
+    }
+
+    bsid = (ptr[5] >> 3) & 0x1f;
+    if (bsid > 10 && bsid <= 16)
+    {
+        frame_size = 2 * ((((ptr[2] << 8) | ptr[3]) & 0x7ff) + 1);
+    }
+    else
+    {
+        frame_size = calc_dd_frame_size(ptr[4]);
+    }
+
+    if (size < frame_size) {
+        ALOGW("Buffer size insufficient for frame size");
+        return NULL;
+    }
+
+    auSize += frame_size;
+
+    // Make Timestamp
+    int64_t timeUs = -1;
+    if(!mRangeInfos.empty())
+        timeUs = fetchTimestamp(frame_size);
+    else
+        ALOGW("Timestamp not created because mRangeInfos was empty");
+
+    // Now create an access unit
+    sp<ABuffer> accessUnit = new ABuffer(auSize);
+    // Put data into buffer
+    memcpy(accessUnit->data(), mBuffer->data(), frame_size);
+
+    memmove(mBuffer->data(), mBuffer->data() + frame_size, mBuffer->size() - frame_size);
+    mBuffer->setRange(0, mBuffer->size() - frame_size);
+
+    accessUnit->meta()->setInt64("timeUs", timeUs);
+    if (timeUs >= 0) {
+        accessUnit->meta()->setInt64("timeUs", timeUs);
+    } else {
+        ALOGW("no time for DDP access unit");
+    }
+
+    return accessUnit;
+}
+#endif // DOLBY_UDC && DOLBY_UDC_STREAMING_HLS
 int64_t ElementaryStreamQueue::fetchTimestamp(size_t size) {
     int64_t timeUs = -1;
     bool first = true;