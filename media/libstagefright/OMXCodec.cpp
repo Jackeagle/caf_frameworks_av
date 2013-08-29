b0c8e4135d44b6c6e224d9ccadd3446e709808fb
Kim Zhang <xiazhang@codeaurora.org>
2013-08-29 17:54:15 +0800

    camcorder pause feature
    --libmedia: add pause() interface in MediaRecorder and IMediaRecorder and add Pause state in mediarecorder's state machine
    --libmediaplayerservice: add pause() interface in MediaRecorderClient and StagefrightRecorder
    --libstagefright:      1.stop-in-pause implementation in recording pipeline for pause feature;
    --camerasource: support switch between start() and stop()
    --audiosource:  support switch between start() and stop()
    --soft aacenc component:  clear some variables when doing flush to support re-start
    --soft amrnbenc component: clear some variables when doing flush to support re-start
    
    Change-Id: I3036ef8f4cc7644c1d36aa0b19e64fc9dc12c1f8

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -4045,6 +4045,7 @@ status_t OMXCodec::start(MetaData *meta) {
             CODEC_LOGE("init failed: %d", err);
             return err;
         }
+        ALOGV("component started!");
 
         params->setInt32(kKeyNumBuffers, mPortBuffers[kPortIndexInput].size());
         err = mSource->start(params.get());
@@ -4067,7 +4068,15 @@ status_t OMXCodec::stop() {
     CODEC_LOGV("stop mState=%d", mState);
     Mutex::Autolock autoLock(mLock);
     status_t err = stopOmxComponent_l();
+    ALOGV("component stopped!");
     mSource->stop();
+    //clear the decodingtime list
+    List<int64_t >::iterator it;
+    while (!mDecodingTimeList.empty()) {
+        it = mDecodingTimeList.begin();
+        mDecodingTimeList.erase(it);
+    }
+    //end
 
     CODEC_LOGV("stopped in state %d", mState);
     return err;
