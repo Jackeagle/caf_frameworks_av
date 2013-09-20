e6c14290d06e2d1c39a6be67e026b6937a092526
Michael Vines <mvines@codeaurora.org>
2013-09-20 10:39:01 -0700

    OMXCodec::stop() now waits for all buffers to return
    Ref: https://bugzilla.mozilla.org/show_bug.cgi?id=823324
    
    Change-Id: Ie8fab02b59183c9ac6e4135cfb5a79bb2321493e

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -1518,6 +1518,7 @@ OMXCodec::OMXCodec(
       mTargetTimeUs(-1),
       mOutputPortSettingsChangedPending(false),
       mSkipCutBuffer(NULL),
+      mNumClientBuffers(0),
       mLeftOverBuffer(NULL),
       mPaused(false),
       mNativeWindow(
@@ -3940,6 +3941,10 @@ status_t OMXCodec::stopOmxComponent_l() {
         mAsyncCompletion.wait(mLock);
     }
 
+    while (mNumClientBuffers) {
+        mBufferCompletion.wait(mLock);
+    }
+
     bool isError = false;
     switch (mState) {
         case LOADED:
@@ -4190,6 +4195,7 @@ status_t OMXCodec::read(
 
     BufferInfo *info = &mPortBuffers[kPortIndexOutput].editItemAt(index);
     CHECK_EQ((int)info->mStatus, (int)OWNED_BY_US);
+    mNumClientBuffers++;
     info->mStatus = OWNED_BY_CLIENT;
 
     info->mMediaBuffer->add_ref();
@@ -4216,7 +4222,9 @@ void OMXCodec::signalBufferReturned(MediaBuffer *buffer) {
             CHECK_EQ((int)mPortStatus[kPortIndexOutput], (int)ENABLED);
             CHECK_EQ((int)info->mStatus, (int)OWNED_BY_CLIENT);
 
+            mNumClientBuffers--;
             info->mStatus = OWNED_BY_US;
+            mBufferCompletion.signal();
 
             if (buffer->graphicBuffer() == 0) {
                 fillOutputBuffer(info);
