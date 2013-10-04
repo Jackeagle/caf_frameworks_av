71f625cc3980b71450a345fe3825c0574a2d66db
Tapas Kumar Kundu <tkundu@codeaurora.org>
2013-10-04 12:37:23 -0700

    Avoid IAppOpsService so that fakeappops can be removed.
    Change-Id: I70e86ffa1b3e83b49a076bac77d019ab61b1144b

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -986,10 +986,7 @@ status_t CameraService::BasicClient::startCameraOps() {
               __FUNCTION__, String8(mClientPackageName).string(), mClientUid);
     }
 
-    mAppOpsManager.startWatchingMode(AppOpsManager::OP_CAMERA,
-            mClientPackageName, mOpsCallback);
-    res = mAppOpsManager.startOp(AppOpsManager::OP_CAMERA,
-            mClientUid, mClientPackageName);
+    res = AppOpsManager::MODE_ALLOWED;
 
     if (res != AppOpsManager::MODE_ALLOWED) {
         ALOGI("Camera %d: Access for \"%s\" has been revoked",
@@ -1002,11 +999,8 @@ status_t CameraService::BasicClient::startCameraOps() {
 
 status_t CameraService::BasicClient::finishCameraOps() {
     if (mOpsActive) {
-        mAppOpsManager.finishOp(AppOpsManager::OP_CAMERA, mClientUid,
-                mClientPackageName);
         mOpsActive = false;
     }
-    mAppOpsManager.stopWatchingMode(mOpsCallback);
     mOpsCallback.clear();
 
     return OK;
@@ -1022,8 +1016,7 @@ void CameraService::BasicClient::opChanged(int32_t op, const String16& packageNa
     }
 
     int32_t res;
-    res = mAppOpsManager.checkOp(AppOpsManager::OP_CAMERA,
-            mClientUid, mClientPackageName);
+    res = AppOpsManager::MODE_ALLOWED;
     ALOGV("checkOp returns: %d, %s ", res,
             res == AppOpsManager::MODE_ALLOWED ? "ALLOWED" :
             res == AppOpsManager::MODE_IGNORED ? "IGNORED" :
