14af24bad78d662f9e03d8547945fd5a04354db6
Surajit Podder <spodder@codeaurora.org>
2013-10-10 19:21:55 +0530

    libstagefright: Disable multi slice mode
    Disable multi slice mode video encoding based on platform.
    
    CRs-Fixed: 546591
    Change-Id: Ib986e418c68013ba9ea09ecd1c735396565c9f5b

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -1135,6 +1135,7 @@ status_t OMXCodec::setupErrorCorrectionParameters() {
     errorCorrectionType.nResynchMarkerSpacing = 256;
     errorCorrectionType.bEnableDataPartitioning = OMX_FALSE;
     errorCorrectionType.bEnableRVLC = OMX_FALSE;
+    ExtendedCodec::overrideErrorCorrectionParameters(errorCorrectionType);
 
     err = mOMX->setParameter(
             mNode, OMX_IndexParamVideoErrorCorrection,
