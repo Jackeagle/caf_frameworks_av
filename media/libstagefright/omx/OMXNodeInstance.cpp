Jackeagle/caf_frameworks_base

Allow OMXCodec to be shutdown in paused state
See https://bugzilla.mozilla.org/show_bug.cgi?id=871018 for details.

Change-Id: I38d3e5f73c9aa145c6f467b7ed2c3b8a1d6e34fd

media/libstagefright/omx/OMXNodeInstance.cpp
@@ -143,6 +143,9 @@ status_t OMXNodeInstance::freeNode(OMXMaster *master) {
     OMX_STATETYPE state;
     CHECK_EQ(OMX_GetState(mHandle, &state), OMX_ErrorNone);
     switch (state) {
+        case OMX_StatePause:
+            LOGV("forcing Pause->Idle");
+            // fall through
         case OMX_StateExecuting:
         {
             LOGV("forcing Executing->Idle");

Jackeagle/caf_frameworks_av

media/libstagefright/omx/OMXNodeInstance.cpp
@@ -155,6 +155,9 @@ status_t OMXNodeInstance::freeNode(OMXMaster *master) {
     OMX_STATETYPE state;
     CHECK_EQ(OMX_GetState(mHandle, &state), OMX_ErrorNone);
     switch (state) {
+        case OMX_StatePause:
+            ALOGV("forcing Pause->Idle");
+            // fall through
         case OMX_StateExecuting:
         {
             ALOGV("forcing Executing->Idle");