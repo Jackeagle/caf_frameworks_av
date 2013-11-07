302a7355cb2c9fe69d81da72df849773403eaa0d
Surajit Podder <spodder@codeaurora.org>
2013-11-07 16:47:03 +0530

    libstagefright: Drain input buffer on resume
    * Buffers returned from codec in paused state are
      not drained. When codec is resumed these buffers
      are not drained until the next flush, and may
      cause timed out issue.
    * Added change to drain input buffers for sw
      decoders when resuming.
    
    CRs-Fixed: 569585
    CRs-Fixed: 574967
    Change-Id: Ia62f03e025310457df57e3106575fcc011e64a54

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -5020,13 +5020,13 @@ status_t OMXCodec::resumeLocked(bool drainInputBuf) {
         while (mState != EXECUTING && mState != ERROR) {
             mAsyncCompletion.wait(mLock);
         }
-        if(drainInputBuf)
-            drainInputBuffers();
         return mState == ERROR ? UNKNOWN_ERROR : OK;
     } else {   // SW Codec
         mPaused = false;
         return OK;
     }
+    if(drainInputBuf)
+        drainInputBuffers();
 }
 
 status_t OMXCodec::updateConcurrencyParam(bool pauseflag) {
