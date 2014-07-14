ea0402392e915031354349e67a037e94272a9d14
Surajit Podder <spodder@codeaurora.org>
2014-07-14 16:52:50 +0530

    video: Fix crash during video recording
    In pause-resume scenario, MPEG4Writer::start is
    called to resume recording, which also resets
    start time for writer to -1. If this happens
    between two tracks updating the writer start
    time, it can end up having the larger of the
    start time of the two tracks, which results in
    crash later due to assert.
    
    Added change to set mStartTimestampUs to -1 only
    when writer is being started, and not resuming from
    pause.
    
    CRs-Fixed: 692122
    Change-Id: Ia98656acfa3912876d01e92b36c5e06a8c84eb7b

--- media/libstagefright/MPEG4Writer.cpp
+++ media/libstagefright/MPEG4Writer.cpp
@@ -615,8 +615,6 @@ status_t MPEG4Writer::start(MetaData *param) {
         mIsRealTimeRecording = isRealTimeRecording;
     }
 
-    mStartTimestampUs = -1;
-
     if (mStarted) {
         if (mPaused) {
             mPaused = false;
@@ -624,6 +622,8 @@ status_t MPEG4Writer::start(MetaData *param) {
         }
         return OK;
     }
+
+    mStartTimestampUs = -1;
 
     if (!param ||
         !param->findInt32(kKeyTimeScale, &mTimeScale)) {
