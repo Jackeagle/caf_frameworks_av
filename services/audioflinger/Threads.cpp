d0e064b9e801ef69daa1274df9f296855848c774
Vasanthakumar Pandurangan <vasanth@codeaurora.org>
2014-09-12 19:02:20 +0530

    Use sys wake_lock interface directly in B2G
    AudioFlinger uses android power manager service which is
    not available in B2G. Use wake_lock sysfs interface
    instead
    
    Conflicts:
    	services/audioflinger/Threads.cpp
    
    Change-Id: Ib492e685b9e9be9c5fb4ad14966a8e321db1d2c4

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -74,6 +74,7 @@
 #include <media/nbaio/SourceAudioBufferProvider.h>
 
 #include <powermanager/PowerManager.h>
+#include <hardware_legacy/power.h>
 
 #include <common_time/cc_helper.h>
 #include <common_time/local_clock.h>
@@ -949,6 +950,9 @@ void AudioFlinger::ThreadBase::acquireWakeLock_l(int uid)
             mWakeLockToken = binder;
         }
         ALOGV("acquireWakeLock_l() %s status %d", mThreadName, status);
+    } else {
+        acquire_wake_lock(PARTIAL_WAKE_LOCK, mThreadName);
+        ALOGV("acquireWakeLock_l() %s", mThreadName);
     }
 }
 
@@ -967,6 +971,9 @@ void AudioFlinger::ThreadBase::releaseWakeLock_l()
                     true /* FIXME force oneway contrary to .aidl */);
         }
         mWakeLockToken.clear();
+    } else {
+        ALOGV("releaseWakeLock_l() %s", mThreadName);
+        release_wake_lock(mThreadName);
     }
 }
 
