de50e7206dffaddae2f12f59b8ef4a3f3fd8bd6f
Leena Winterrowd <lenhardw@codeaurora.org>
2014-09-24 14:49:17 -0700

    libstagefright: Allow multi-byte CSD sizes for non-m4v clips
    As per ISO/IEC 14496-15, there is no explicit limit on the size of
    codec-specific-data for AVC clips. Only mpeg4 video requires that
    CSD size must fit within a single byte. Make the assertion enforcing
    this condition specific to m4v content.
    
    CRs-Fixed: 724526
    Change-Id: Id006030ac65d5ec0bf285c040e4c536039f9489b
    (cherry picked from commit ff49abac9260b7ae0a509a4d0bf4b5899b2d776e)

--- media/libstagefright/MPEG4Writer.cpp
+++ media/libstagefright/MPEG4Writer.cpp
@@ -2718,8 +2718,6 @@ void MPEG4Writer::Track::writeVideoFourCCBox() {
     mOwner->writeInt16(0x18);        // depth
     mOwner->writeInt16(-1);          // predefined
 
-    CHECK_LT(23 + mCodecSpecificDataSize, 128);
-
     if (!strcasecmp(MEDIA_MIMETYPE_VIDEO_MPEG4, mime)) {
         writeMp4vEsdsBox();
     } else if (!strcasecmp(MEDIA_MIMETYPE_VIDEO_H263, mime)) {
@@ -2816,6 +2814,9 @@ void MPEG4Writer::Track::writeMp4vEsdsBox() {
     CHECK(mCodecSpecificData);
     CHECK_GT(mCodecSpecificDataSize, 0);
     mOwner->beginBox("esds");
+
+    // Make sure all sizes encode to a single byte.
+    CHECK_LT(mCodecSpecificDataSize + 23, 128);
 
     mOwner->writeInt32(0);    // version=0, flags=0
 
