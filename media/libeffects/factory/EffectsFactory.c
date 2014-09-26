1b366ae71eff36718dabef2d108598bdccc57fac
Haynes Mathew George <hgeorge@codeaurora.org>
2014-09-26 16:26:21 -0700

    effects: miscellaneous fixes
    fixes of KW issues 8887, 8888
    
    Change-Id: I824daa76d23456fed2c299c59f934d3e2ac78b84

--- media/libeffects/factory/EffectsFactory.c
+++ media/libeffects/factory/EffectsFactory.c
@@ -587,6 +587,11 @@ int addSubEffect(cnode *root)
         return -EINVAL;
     }
     d = malloc(sizeof(effect_descriptor_t));
+    if (!d) {
+        ALOGE("failed to allocate effect descriptor");
+        return -EINVAL;
+    }
+
     if (l->desc->get_descriptor(&uuid, d) != 0) {
         char s[40];
         uuidToString(&uuid, s, 40);
@@ -660,8 +665,12 @@ int loadEffect(cnode *root)
         ALOGW("loadEffect() invalid uuid %s", node->value);
         return -EINVAL;
     }
-
     d = malloc(sizeof(effect_descriptor_t));
+    if (!d) {
+        ALOGE("failed to allocate effect descriptor");
+        return -EINVAL;
+    }
+
     if (l->desc->get_descriptor(&uuid, d) != 0) {
         char s[40];
         uuidToString(&uuid, s, 40);
