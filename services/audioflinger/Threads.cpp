aaca48a89ab6af167bcb56bf4bb7a08b523500db
Manish Dewangan <manish@codeaurora.org>
2014-11-14 20:44:54 +0530

    Revert "audio: compress offload to pcm fallback on SSR"
    This reverts commit c699f5dc7444102b7704d0515ee569d94797dc3f.
    
    -Offload playback is not working on Nuplayer as
    compress SSR is not handled in Nuplayer.
    
    -Revertig this change to unblock APT testing till proper fix to
    handle compress SSR in Nuplayer is identified.
    
    Change-Id: I0fef8b801c3ab8f3742ec46a1221a03cdd391118

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -4652,9 +4652,12 @@ void AudioFlinger::OffloadThread::onAddNewTrack_l()
 void AudioFlinger::OffloadThread::onFatalError()
 {
     Mutex::Autolock _l(mLock);
-
-   // call invalidate, to recreate track on fatal error
-   invalidateTracks_l(AUDIO_STREAM_MUSIC);
+    size_t size = mTracks.size();
+    for (size_t i = 0; i < size; i++) {
+        sp<Track> t = mTracks[i];
+        t->signalError();
+    }
+    invalidateTracks_l(AUDIO_STREAM_MUSIC);
 }
 
 // ----------------------------------------------------------------------------
