7dd79376894d1ff70f16fce075b0936a1186f76b
Satya Krishna Pindiproli <satyak@codeaurora.org>
2014-12-16 15:19:30 +0530

    audio: Define mimetype and add AIFF as a valid media format
    Define the mimetype for AIFF container and add ".aif/.aiff" files to
    stagefright scanner for them to be recognized by mediaplayer.
    
    CRs-Fixed: 802228
    Change-Id: I11c0d7c292c8175b7f71b3d6fddbc49ce3dee15d

--- media/libstagefright/ExtendedMediaDefs.cpp
+++ media/libstagefright/ExtendedMediaDefs.cpp
@@ -1,4 +1,4 @@
-/*Copyright (c) 2012 - 2014, The Linux Foundation. All rights reserved.
+/*Copyright (c) 2012 - 2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -61,5 +61,6 @@ const char *MEDIA_MIMETYPE_CONTAINER_QCOGG = "video/qc-ogg";
 const char *MEDIA_MIMETYPE_CONTAINER_QCFLV = "video/qc-flv";
 const char *MEDIA_MIMETYPE_VIDEO_VPX = "video/x-vnd.on2.vp8"; //backward compatibility
 const char *MEDIA_MIMETYPE_CONTAINER_QTIFLAC = "audio/qti-flac";
+const char *MEDIA_MIMETYPE_CONTAINER_QTIAIFF = "audio/x-aiff";
 
 }  // namespace android
