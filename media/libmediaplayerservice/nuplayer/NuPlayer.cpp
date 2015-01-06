a21dfefab4429cd1e8f12f77df8c04d7361eaeef
Preetam Singh Ranawat <apranawat@codeaurora.org>
2015-01-06 16:20:23 +0530

    NuPlayer : send the correct streaming info while opening audio sink
     -For any offload playback NuPlayerRenderer always open the audio sink
      with isStreaming info as true.
     -Pass the streaming info to the NuPlayerRenderer while opening audio
      sink.
    
    CRs-Fixed : 777242
    Change-Id: Ic9d5e7ae0b82a316ec4b4a72037519448f1a011c

--- media/libmediaplayerservice/nuplayer/NuPlayer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayer.cpp
@@ -1168,7 +1168,7 @@ void NuPlayer::openAudioSink(const sp<AMessage> &format, bool offloadOnly) {
     }
 
     mOffloadAudio = mRenderer->openAudioSink(
-            format, offloadOnly, hasVideo, flags);
+            format, offloadOnly, hasVideo, mIsStreaming, flags);
 
     if (mOffloadAudio) {
         sp<MetaData> audioMeta =
