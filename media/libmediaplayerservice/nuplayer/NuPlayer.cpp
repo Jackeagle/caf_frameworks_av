72000d26f0995a82f63fcb37412ad72dd1d4fb10
Sharad Sangle <assangle@codeaurora.org>
2015-02-27 13:02:52 +0530

    NuPlayer:Use video format to check if content has video
    - When video decoder is flushed as a result of non-supported
      video format, NuPlayer tries to open audio sink as format change
      is triggred, NuPlayer tries to open another offload session
      as it assumes there is no video in the stream, as the video
      video decoder is flushed and is null
    
    - Instead of checking video decoder, check for video format
      from source, as it has correct information of audio and
      video streams
    
    Change-Id: I1553689a397962508467f6e8067e06aacdd89600

--- media/libmediaplayerservice/nuplayer/NuPlayer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayer.cpp
@@ -1202,7 +1202,8 @@ void NuPlayer::postScanSources() {
 void NuPlayer::openAudioSink(const sp<AMessage> &format, bool offloadOnly) {
     uint32_t flags;
     int64_t durationUs;
-    bool hasVideo = (mVideoDecoder != NULL);
+    sp<AMessage> videoFormat = mSource->getFormat(false /* video */);
+    bool hasVideo = (videoFormat != NULL);
     // FIXME: we should handle the case where the video decoder
     // is created after we receive the format change indication.
     // Current code will just make that we select deep buffer
