4fca35f4ef651b50e43e974f522cfd711a7a0024
Jitendra Naruka <jitendra.naruka@dts.com>
2015-03-16 13:29:00 -0700

    Revert "mpeg2ts: Add DTSHD HLS streaming and DTSHD detection in MPEG2 PS"
    This reverts commit 9c0046674abe11589b0cb87df15764e8c3a0f59a.

--- media/libstagefright/mpeg2ts/MPEG2PSExtractor.cpp
+++ media/libstagefright/mpeg2ts/MPEG2PSExtractor.cpp
@@ -31,23 +31,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * This file was modified by DTS, Inc. The portions of the
- * code that are surrounded by "DTS..." are copyrighted and
- * licensed separately, as follows:
- *
- *  (C) 2015 DTS, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 //#define LOG_NDEBUG 0
@@ -589,11 +572,7 @@ MPEG2PSExtractor::Track::Track(
         case ATSParser::STREAMTYPE_MPEG2_AUDIO:
             mode = ElementaryStreamQueue::MPEG_AUDIO;
             break;
-#ifdef DTS_CODEC_M_
-        case ATSParser::STREAMTYPE_DTSHD:
-            mode = ElementaryStreamQueue::DTSHD;
-            break;
-#endif
+
 #if defined(DOLBY_UDC) && defined(DOLBY_UDC_STREAMING_HLS)
         case ATSParser::STREAMTYPE_AC3:
             mode = ElementaryStreamQueue::AC3;
