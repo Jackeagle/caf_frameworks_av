123be627e09be66c5e8cb235a4216f60008f8745
Weiyin Jiang <wjiang@codeaurora.org>
2015-05-26 13:08:10 +0800

    nuplayer: start or stop audio sink from renderer only
    NuPlayerRenderer maintains the state of AudioSink, and based on that
    data is written to the audioSink.
    
    But in certain cases, AudioSink is directly accessed by NuPlayerDriver,
    which causes a mismatch in the state maintained in the renderer.
    
    Ensure that audiosink is start/stop from renderer only.
    
    CRs-Fixed: 840811
    Change-Id: I663c876a7e6d529aabb44fc8768cc657a1b228d0

--- media/libmediaplayerservice/nuplayer/NuPlayer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayer.cpp
@@ -1385,6 +1385,10 @@ void NuPlayer::tryOpenAudioSinkForOffload(const sp<AMessage> &format, bool hasVi
     }
 }
 
+void NuPlayer::startAudioSink() {
+    mRenderer->startAudioSink();
+}
+
 void NuPlayer::closeAudioSink() {
     mRenderer->closeAudioSink();
 }
