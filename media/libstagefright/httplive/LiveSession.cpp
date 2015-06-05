cea78cde0741aaf57c923de2bb1a74f511c2d557
Apurupa Pattapu <apurupa@codeaurora.org>
2015-06-05 10:22:44 -0700

    httplive: Check for NULL playlist in postPrepared
    - If playlist parsing fails mPlaylist can be NULL in
      prepare state. Add a NULL check for mPlaylist.
    
    CRs-Fixed: 845920
    
    Change-Id: I239c0e17d4a3c8f7f7429b5ccb82dbb3d408ced2

--- media/libstagefright/httplive/LiveSession.cpp
+++ media/libstagefright/httplive/LiveSession.cpp
@@ -1790,7 +1790,7 @@ void LiveSession::postPrepared(status_t err) {
     mInPreparationPhase = false;
 
     //start switchdown monitor only for variant playlists
-    if (mPlaylist->isVariantPlaylist()) {
+    if (mPlaylist != NULL && mPlaylist->isVariantPlaylist()) {
         mSwitchDownMonitor = new AMessage(kWhatCheckSwitchDown, id());
         mSwitchDownMonitor->post();
     }
