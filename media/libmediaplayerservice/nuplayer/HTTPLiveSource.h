43763bce5b6ae65b8490341885bb9f770d5de84e
Li Sun <sunli@codeaurora.org>
2015-07-14 16:31:19 +0800

    httplive: force format change to fix embedded image display
    - the embedded image was displayed by native window in audio only clip.
      if audio only is switched to audio/video session, video decoder needs
      to be changed to reconfigure the native window.
    
    - remove the unused variables in NuPlayer.
    
    Change-Id: Ib2bc8805d16ff06abc8bdb57476ad35a12da688d
    CRs-Fixed: 872593

--- media/libmediaplayerservice/nuplayer/HTTPLiveSource.h
+++ media/libmediaplayerservice/nuplayer/HTTPLiveSource.h
@@ -72,6 +72,7 @@ private:
     sp<ALooper> mLiveLooper;
     sp<LiveSession> mLiveSession;
     int32_t mFetchSubtitleDataGeneration;
+    bool mImageDisplay;
 
     void onSessionNotify(const sp<AMessage> &msg);
 
