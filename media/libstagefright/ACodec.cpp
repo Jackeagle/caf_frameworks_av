361d6002c32d9b6b911276d27c306b67068be607
Chong Zhang <chz@google.com>
2015-08-14 13:50:02 -0700

    do not dequeue from native window after we hit fatal error -- DO NOT MERGE
    bug: 22845824.
    Change-Id: I8c375790c697e02b6ab3ea54b84d3f70d5e78141
    (cherry picked from commit 346de3c26a8fbd0fa0c8102f4a21ea4dcee4432a)

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -455,6 +455,7 @@ ACodec::ACodec()
       mSentFormat(false),
       mIsEncoder(false),
       mUseMetadataOnEncoderOutput(false),
+      mFatalError(false),
       mShutdownInProgress(false),
       mExplicitShutdown(false),
       mEncoderDelay(0),
@@ -1006,6 +1007,11 @@ ACodec::BufferInfo *ACodec::dequeueBufferFromNativeWindow() {
     if (mTunneled) {
         ALOGW("dequeueBufferFromNativeWindow() should not be called in tunnel"
               " video playback mode mode!");
+        return NULL;
+    }
+
+    if (mFatalError) {
+        ALOGW("not dequeuing from native window due to fatal error");
         return NULL;
     }
 
@@ -4090,6 +4096,9 @@ void ACodec::signalError(OMX_ERRORTYPE error, status_t internalError) {
             ALOGW("Invalid OMX error %#x", error);
         }
     }
+
+    mFatalError = true;
+
     notify->setInt32("err", internalError);
     notify->setInt32("actionCode", ACTION_CODE_FATAL); // could translate from OMX error.
     notify->post();
