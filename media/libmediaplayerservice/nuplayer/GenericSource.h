85ff7b1e32308481884bfc6698a282c3eed305fc
Robert Shih <robertshih@google.com>
2015-09-02 14:02:47 -0700

    DO NOT MERGE: Protect data source access with mutex during disconnect.
    Bug: 23658148
    Change-Id: Ic37cac7b5d166143e0b77e9919b0aaef486e4fdd

--- media/libmediaplayerservice/nuplayer/GenericSource.h
+++ media/libmediaplayerservice/nuplayer/GenericSource.h
@@ -152,6 +152,7 @@ private:
     bool mBuffering;
     bool mPrepareBuffering;
     mutable Mutex mReadBufferLock;
+    mutable Mutex mDisconnectLock;
 
     sp<ALooper> mLooper;
     bool mQueryAndSetProxy;
