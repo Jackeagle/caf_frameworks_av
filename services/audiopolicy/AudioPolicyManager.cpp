9f25b313971fa0aaa1ffb7d9917f5f24048558ef
Venkata Narendra Kumar Gutta <vgutta@codeaurora.org>
2015-09-03 20:13:20 +0530

    audio: Fix VoIP direct output profile selection
    Ringtone mute is observed in LINE app MT call over BT headset.
    VoIP direct output isn't returned even though the required flags
    are set. In this scenario, DIRECT flags are set but track is
    getting primary output. This is resulting in incorrect values
    when renderer queries for getPosition(). Fix this to return
    VoIP direct output when the appropriate flags are set.
    
    CRs-Fixed: 893558
    Change-Id: I4fe8bc05455e337c24452e49be20ec36eec9c85d

--- services/audiopolicy/AudioPolicyManager.cpp
+++ services/audiopolicy/AudioPolicyManager.cpp
@@ -1273,18 +1273,25 @@ sp<AudioPolicyManager::IOProfile> AudioPolicyManager::getProfileForDirectOutput(
                                                                audio_channel_mask_t channelMask,
                                                                audio_output_flags_t flags)
 {
+
+    audio_output_flags_t direct_flags = AUDIO_OUTPUT_FLAG_DIRECT;
+
+    //if the requested flags has VoIP and DIRECT set, update direct_flags to get VoIP direct o/p profile
+    if (flags & (AUDIO_OUTPUT_FLAG_VOIP_RX | AUDIO_OUTPUT_FLAG_DIRECT)) {
+        direct_flags = (audio_output_flags_t) (AUDIO_OUTPUT_FLAG_VOIP_RX | AUDIO_OUTPUT_FLAG_DIRECT);
+    }
+#ifdef HDMI_PASSTHROUGH_ENABLED
+    if (flags & AUDIO_OUTPUT_FLAG_COMPRESS_PASSTHROUGH) {
+        direct_flags = (audio_output_flags_t) (AUDIO_OUTPUT_FLAG_COMPRESS_PASSTHROUGH | AUDIO_OUTPUT_FLAG_DIRECT);
+    }
+#endif
+
     for (size_t i = 0; i < mHwModules.size(); i++) {
         if (mHwModules[i]->mHandle == 0) {
             continue;
         }
         for (size_t j = 0; j < mHwModules[i]->mOutputProfiles.size(); j++) {
             sp<IOProfile> profile = mHwModules[i]->mOutputProfiles[j];
-            audio_output_flags_t direct_flags = AUDIO_OUTPUT_FLAG_DIRECT;
-#ifdef HDMI_PASSTHROUGH_ENABLED
-            if (flags & AUDIO_OUTPUT_FLAG_COMPRESS_PASSTHROUGH) {
-                direct_flags = AUDIO_OUTPUT_FLAG_COMPRESS_PASSTHROUGH | AUDIO_OUTPUT_FLAG_DIRECT;
-            }
-#endif
             bool found = profile->isCompatibleProfile(device, String8(""), samplingRate,
                     NULL /*updatedSamplingRate*/, format, channelMask,
                     flags & AUDIO_OUTPUT_FLAG_COMPRESS_OFFLOAD ?
