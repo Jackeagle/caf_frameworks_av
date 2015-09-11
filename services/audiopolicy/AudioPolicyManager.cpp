92611184f5fd49605be64f3db25cd60906aa431f
Venkata Narendra Kumar Gutta <vgutta@codeaurora.org>
2015-09-11 12:10:21 +0530

    audio: update getDevice() function to address profile selection
    Update getDevice() function to return device when the requested device
    is present in the output/input profile for a given hw module.
    For example, return device when the requested device is
    AUDIO_DEVICE_OUT_BLUETOOTH_SCO_HEADSET and the profile contains
    AUDIO_DEVICE_ DEVICE_OUT_ALL_SCO.
    
    CRs-Fixed: 893558
    Change-Id: If8a3174468e1f82b68e4c3e96fcd1eaa6befd9c1

--- services/audiopolicy/AudioPolicyManager.cpp
+++ services/audiopolicy/AudioPolicyManager.cpp
@@ -8817,7 +8817,7 @@ sp<AudioPolicyManager::DeviceDescriptor> AudioPolicyManager::DeviceVector::getDe
 {
     sp<DeviceDescriptor> device;
     for (size_t i = 0; i < size(); i++) {
-        if (itemAt(i)->mDeviceType == type) {
+        if ((itemAt(i)->mDeviceType) & type) {
             if (address == "" || itemAt(i)->mAddress == address) {
                 device = itemAt(i);
                 if (itemAt(i)->mAddress == address) {
