673e20d7745ab10c36a6f6dcece365e023f2fd3c
Marco Nelissen <marcone@google.com>
2016-02-29 12:47:20 -0800

    DO NOT MERGE Verify OMX buffer sizes prior to access
    Bug: 27207275
    Conflicts:
            media/libmedia/IOMX.cpp
    
    Change-Id: Icfe6103c2f963f6d74c2fbb0a943b1a5064b53ff

--- media/libstagefright/codecs/avc/enc/SoftAVCEncoder.cpp
+++ media/libstagefright/codecs/avc/enc/SoftAVCEncoder.cpp
@@ -366,6 +366,10 @@ OMX_ERRORTYPE SoftAVCEncoder::internalGetParameter(
             OMX_VIDEO_PARAM_BITRATETYPE *bitRate =
                 (OMX_VIDEO_PARAM_BITRATETYPE *) params;
 
+            if (!isValidOMXParam(bitRate)) {
+                return OMX_ErrorBadParameter;
+            }
+
             if (bitRate->nPortIndex != 1) {
                 return OMX_ErrorUndefined;
             }
@@ -379,6 +383,10 @@ OMX_ERRORTYPE SoftAVCEncoder::internalGetParameter(
         {
             OMX_VIDEO_PARAM_AVCTYPE *avcParams =
                 (OMX_VIDEO_PARAM_AVCTYPE *)params;
+
+            if (!isValidOMXParam(avcParams)) {
+                return OMX_ErrorBadParameter;
+            }
 
             if (avcParams->nPortIndex != 1) {
                 return OMX_ErrorUndefined;
@@ -423,6 +431,10 @@ OMX_ERRORTYPE SoftAVCEncoder::internalSetParameter(
             OMX_VIDEO_PARAM_BITRATETYPE *bitRate =
                 (OMX_VIDEO_PARAM_BITRATETYPE *) params;
 
+            if (!isValidOMXParam(bitRate)) {
+                return OMX_ErrorBadParameter;
+            }
+
             if (bitRate->nPortIndex != 1 ||
                 bitRate->eControlRate != OMX_Video_ControlRateVariable) {
                 return OMX_ErrorUndefined;
@@ -436,6 +448,10 @@ OMX_ERRORTYPE SoftAVCEncoder::internalSetParameter(
         {
             OMX_VIDEO_PARAM_AVCTYPE *avcType =
                 (OMX_VIDEO_PARAM_AVCTYPE *)params;
+
+            if (!isValidOMXParam(avcType)) {
+                return OMX_ErrorBadParameter;
+            }
 
             if (avcType->nPortIndex != 1) {
                 return OMX_ErrorUndefined;
