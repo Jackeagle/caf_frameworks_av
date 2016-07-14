c739a0445f0221b8649420d49907e44ac2df9221
Lajos Molnar <lajos@google.com>
2016-07-13 20:27:32 -0700

    fix build: it is cherry-picked from
    commit:50643aadeb8dfe53da7848a5d5e995d5486678e1
    
    Change-Id: I9bb8c659d3fc97a8e748451d82d0f3448faa242b
    (cherry picked from commit 50643aadeb8dfe53da7848a5d5e995d5486678e1)

--- media/libstagefright/codecs/on2/dec/SoftVPX.cpp
+++ media/libstagefright/codecs/on2/dec/SoftVPX.cpp
@@ -185,6 +185,25 @@ bool SoftVPX::outputBuffers(bool flushDecoder, bool display, bool eos, bool *por
     return true;
 }
 
+bool SoftVPX::outputBufferSafe(OMX_BUFFERHEADERTYPE *outHeader) {
+    uint32_t width = outputBufferWidth();
+    uint32_t height = outputBufferHeight();
+    uint64_t nFilledLen = width;
+    nFilledLen *= height;
+    if (nFilledLen > UINT32_MAX / 3) {
+        ALOGE("b/29421675, nFilledLen overflow %llu w %u h %u",
+                (unsigned long long)nFilledLen, width, height);
+        android_errorWriteLog(0x534e4554, "29421675");
+        return false;
+    } else if (outHeader->nAllocLen < outHeader->nFilledLen) {
+        ALOGE("b/27597103, buffer too small");
+        android_errorWriteLog(0x534e4554, "27597103");
+        return false;
+    }
+
+    return true;
+}
+
 void SoftVPX::onQueueFilled(OMX_U32 /* portIndex */) {
     if (mOutputPortSettingsChange != NONE || mEOSStatus == OUTPUT_FRAMES_FLUSHED) {
         return;
