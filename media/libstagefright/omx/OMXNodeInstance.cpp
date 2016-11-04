ccf623534721dcf822382e5b7efc87ec3ee7f6cc
Lajos Molnar <lajos@google.com>
2016-11-04 17:51:11 +0530

    [PATCH 09/11] IOMX: restrict conversion
    of ANWB to gralloc source in emptyBuffer
    
    This is only allowed in-process (if backup and codec buffers are
    connected.)
    Bug: 29422020#
    Bug: 31412859#
    
    Change-Id: Ie081ae29e282298f53bc646d04fe5df4a1be5877

--- media/libstagefright/omx/OMXNodeInstance.cpp
+++ media/libstagefright/omx/OMXNodeInstance.cpp
@@ -170,6 +170,10 @@ struct BufferMeta {
         return buf;
     }
 
+    bool copyToOmx() const {
+         return mCopyToOmx;
+    }
+
     void setGraphicBuffer(const sp<GraphicBuffer> &graphicBuffer) {
         mGraphicBuffer = graphicBuffer;
     }
@@ -787,14 +791,12 @@ status_t OMXNodeInstance::useBuffer(
             delete[] data;
             return BAD_VALUE;
         }
-
         buffer_meta = new BufferMeta(
-                params, portIndex, false /* copyToOmx */, false /* copyFromOmx */, data);
+               params, portIndex, false /* copyToOmx */, false /* copyFromOmx */, data);
     } else {
         buffer_meta = new BufferMeta(
                 params, portIndex, false /* copyFromOmx */, false /* copyToOmx */, NULL);
     }
-
     OMX_BUFFERHEADERTYPE *header;
 
     OMX_ERRORTYPE err = OMX_UseBuffer(
@@ -1281,7 +1283,8 @@ status_t OMXNodeInstance::emptyBuffer(
 
     // convert incoming ANW meta buffers if component is configured for gralloc metadata mode
     // ignore rangeOffset in this case
-    if (mMetadataType[kPortIndexInput] == kMetadataBufferTypeGrallocSource
+    if (buffer_meta->copyToOmx()
+            && mMetadataType[kPortIndexInput] == kMetadataBufferTypeGrallocSource
             && backup->capacity() >= sizeof(VideoNativeMetadata)
             && codec->capacity() >= sizeof(VideoGrallocMetadata)
             && ((VideoNativeMetadata *)backup->base())->eType
