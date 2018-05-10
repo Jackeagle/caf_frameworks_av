94857794aa85bd6091d3a71cdbd63264de14f229
Malathi Gottam <mgottam@codeaurora.org>
2018-05-10 16:02:24 +0530

    libstagefright: fix to retrieve thumbnail for HEIF content
    thumbnail mode hint has been set and
    EOS has been passed.
    
    CRs-Fixed: 2220452 2452304
    
    Change-Id: Iac6d53928c4ebbd731fcb0c50f1d862afb6bb4b4

--- media/libstagefright/StagefrightMetadataRetriever.cpp
+++ media/libstagefright/StagefrightMetadataRetriever.cpp
@@ -211,7 +211,7 @@ sp<IMemory> StagefrightMetadataRetriever::getImageInternal(
     MediaCodecList::findMatchingCodecs(
             mime,
             false, /* encoder */
-            MediaCodecList::kPreferSoftwareCodecs,
+            0/*MediaCodecList::kPreferSoftwareCodecs*/,
             &matchingCodecs);
 
     for (size_t i = 0; i < matchingCodecs.size(); ++i) {
@@ -336,7 +336,7 @@ status_t StagefrightMetadataRetriever::getFrameInternal(
     MediaCodecList::findMatchingCodecs(
             mime,
             false, /* encoder */
-            MediaCodecList::kPreferSoftwareCodecs,
+            0/*MediaCodecList::kPreferSoftwareCodecs*/,
             &matchingCodecs);
 
     for (size_t i = 0; i < matchingCodecs.size(); ++i) {
