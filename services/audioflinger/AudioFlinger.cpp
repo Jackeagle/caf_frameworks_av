4c067aa7c93a3e7a986225f59685871a74165e4b
Jean-Michel Trivi <jmtrivi@google.com>
2019-08-20 15:42:04 -0700

    AudioFlinger: enforce OP_RECORD_AUDIO during recording
      Fix issue where RecordTrack silencing didn't silence the
    full buffer: the memset to 0 was using the RecordThread frame
    size, not the RecordTrack frame size.
      OP_RECORD_AUDIO was only enforced at the start of a recording
    which would fail if not granted. This patch silences the recording
    (i.e. silence is recorded instead) when it is lost, and undoes that
    when granted again. This requires:
    - propagating the package name of the client to the RecordTrack class
    - registering an appOp callback in RecordTrack (through a new
      OpRecordAudioMonitor class) to (un)silence the recording
    - update the isSilenced() method to take into account the appOp.
    
    Bug: 138968594
    Bug: 138636979
    Test: run app that records audio, then "adb shell appops __pack_name__ 27 2"
         and verify recording is silent after that.
    
    Change-Id: I4ebdb2f02409fdf045a77132bf7a1b24249ad948
    Merged-In: Ib33f5b592185a67204997213bab1ac2594d90d37
    (cherry picked from commit 43def76fcdb1d3a9b14a3f19a4c8a33534f8b003)

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -1725,7 +1725,8 @@ sp<media::IAudioRecord> AudioFlinger::createRecord(const CreateRecordInput& inpu
                                                   &output.notificationFrameCount,
                                                   clientUid, &output.flags,
                                                   input.clientInfo.clientTid,
-                                                  &lStatus, portId);
+                                                  &lStatus, portId,
+                                                  input.opPackageName);
         LOG_ALWAYS_FATAL_IF((lStatus == NO_ERROR) && (recordTrack == 0));
 
         // lStatus == BAD_TYPE means FAST flag was rejected: request a new input from
